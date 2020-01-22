4e11e64103929dfb9babcf95f593dd3e5c03929e
Jesus Renteria <Jesus Renteria>
2020-01-22 12:47:07 -0600

    added a view index.js to render the homepage

---
+++ routes/view/index.js
@@ -0,0 +1,20 @@
+var router = require("express").Router();
+var db = require("../../models");
+
+router.get("/", function(req, res){
+    db.Headline.find({ saved: false})
+    .sort({ date: -1})
+    .then(function(dbArticles){
+        res.render("home", { articles: dbArticles});
+    });
+});
+
+router.get("/saved", function(req, res) {
+    db.Headline.find({ saved: true })
+      .sort({ date: -1 })
+      .then(function(dbArticles) {
+        res.render("saved", { articles: dbArticles });
+      });
+  });
+
+  module.exports = router;
